e9602979ac2d89aecca6fc34561c65612c8ce734
MD RAQUIB HUSSAIN <mdraquibhussain71@gmail.com>
2026-06-03 00:26:14 +0530

    last commit

---
+++ Revision/script.js
@@ -0,0 +1,68 @@
+// Level 1: BASIC(10 QUESTIONS)
+
+// set1
+// Qs1
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",()=>{
+// console.log("hello world")
+// })
+
+// Qs2
+// let head=document.getElementById("head");
+// head.textContent="Hello World";
+
+// Qs3
+// let para=document.querySelector("p")
+
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",()=>{
+// para.style.color="red"
+// });
+
+// Qs4
+// let div=document.querySelector("div");
+// div.style.width="200px";
+// div.style.height="200px";
+// div.style.backgroundColor="yellow";
+
+// Qs5
+// let inp=document.querySelector("input");
+// let btn=document.querySelector("button");
+// let val;
+// inp.addEventListener("input",(e)=>{
+// val=e.target.value;
+// })
+// btn.addEventListener("click",()=>{
+// console.log(val);
+// })
+
+// Qs6
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",()=>{
+// document.querySelector("img").src=" ";//OR
+// document.querySelector("img").style.background="none";
+// })
+
+// Qs7
+// let btn=document.querySelector("button");
+// document.querySelector("img").hidden=true;
+// btn.addEventListener("click",()=>{
+// document.querySelector("img").hidden=false
+// })
+
+// Qs8
+// let h2=document.querySelector("h2");//"Hello World"
+// h2.innerText="Welcome to the DOM";
+
+// Qs9
+// let btn=document.querySelector("button");
+// btn.addEventListener("click",()=>{
+//     document.body.style.backgroundColor="lightblue";
+// })
+
+// Qs10
+// let button=document.querySelector("button");
+//   let count=document.querySelector("span");
+// button.addEventListener("click",()=>{
+//     count.textContent=parseInt(count.textContent) + 1;
+// })
